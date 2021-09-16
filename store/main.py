10edfd66c4b35f14c1b60ad507a9b95b687e97da
oxmc <67136658+oxmc@users.noreply.github.com>
2021-09-15 20:08:38 -0700

    Update main.py

--- store/main.py
+++ store/main.py
@@ -19,40 +19,26 @@ import getpass
 global username
 username = getpass.getuser()
 
-
 #Import custom  pi-ware functions
 from apps import *
-
 import sys
-
-
 
 def featuredTab(parent):
      widget = QWidget(parent)
-     
      layout = QVBoxLayout(widget)
-
      browser = QWebView(widget)
      browser.showMaximized()
      browser.setUrl(QUrl("http://pi-ware-telemetry.ml/widget-2/"))
      layout.addWidget(browser)
      widget.setLayout(layout)
-
      return widget
-     
-
-
 
 def appsTab(parent):
-
      widget = QWidget(parent)
      scrollbar_widget = QWidget(widget)
-     
      scroll_layout = QVBoxLayout(scrollbar_widget)
-
      apps = parseApps()
      appWindows = list()
-
      for app in apps:                                          
           btn = QPushButton(app.name, widget)            
           text = btn.text()
@@ -60,48 +46,33 @@ def appsTab(parent):
           btn.app = AppWindow(app)
           btn.clicked.connect(btn.app.show)
           scroll_layout.addWidget(btn)
-
-        
      scroll_layout.addStretch(1)
-
      scroll = QScrollArea()
      scroll.setWidget(scrollbar_widget)
      scroll.setWidgetResizable(True)
-
      layout = QVBoxLayout(widget)
      layout.addWidget(scroll)
-     
      widget.setLayout(layout)
-
      return widget
-
 
 def wikiTab(parent):
      widget = QWidget(parent)
-     
      layout = QVBoxLayout(widget)
-
      browser = QWebView(widget)
      browser.showMaximized()
      browser.setUrl(QUrl("http://github.com/piware14/pi-ware/wiki"))
      layout.addWidget(browser)
      widget.setLayout(layout)
-
      return widget
-
 
 class PiWare(QMainWindow):
      def __init__(self, x):
          super(PiWare, self).__init__()
          #self.resize(300, 800)
-
-         self.setWindowIcon(QtGui.QIcon(f'/home/{username}/pi-ware/icons/logo-full.png'))
+         self.setWindowIcon(QtGui.QIcon(f'/home/{username}/pi-ware-pyqt5/icons/logo-full.png'))
          self.setWindowTitle("Pi-Ware")
-
          self.table_widget = Tabs(self)
-         self.setCentralWidget(self.table_widget)
-
-                  
+         self.setCentralWidget(self.table_widget)      
 
 class Tabs(QTabWidget):
    def __init__(self, parent = None):
@@ -109,63 +80,40 @@ class Tabs(QTabWidget):
       self.featuredTab = featuredTab(self)
       self.appsTab = appsTab(self)
       self.wikiTab = wikiTab(self)
-		
       self.addTab(self.featuredTab,"Featured")
       self.addTab(self.appsTab,"Apps")
       self.addTab(self.wikiTab,"Wiki")
-      
-		
-		
-
 
 class AppWindow(QWidget):
-    """
-    This "window" is a QWidget. If it has no parent, it
-    will appear as a free-floating window as we want.
-    """
     def __init__(self, app: App):
         super().__init__()
         layout = QGridLayout()
         self.install_script = app.install
         self.uninstall_script = app.uninstall
-
-        
         self.nameLabel = QLabel(app.name)
         self.nameLabel.setFont(QFont('Arial', 30))
         layout.addWidget(self.nameLabel, 0, 0)
-
-        
         logoLabel = QLabel(self)
         logoPixmap = QPixmap(app.icon200)
         logoLabel.setPixmap(logoPixmap)
         layout.addWidget(logoLabel, 1, 0)
-
         archLabel = QLabel("Architecture: "+app.architecture)
         layout.addWidget(archLabel, 0, 1)
-
         descLabel = QLabel("\n"+app.description+"\n\n"+app.website)
         layout.addWidget(descLabel, 1, 1)
-
         installButton = QPushButton("Install")
         installButton.clicked.connect(self.install)
         layout.addWidget(installButton, 2, 0)
-
         uninstallButton = QPushButton("Uninstall")
         uninstallButton.clicked.connect(self.uninstall)
         layout.addWidget(uninstallButton, 2, 1)
- 
-        
         self.setLayout(layout)
 
-
     def install(self):
-         subprocess.Popen(["bash", f"/home/{username}/pi-ware/func/term/term-run", str(self.install_script)])
-
+         subprocess.Popen(["bash", f"/home/{username}/pi-ware-pyqt5/func/term/term-run", str(self.install_script)])
 
     def uninstall(self):
-
-        subprocess.Popen(["bash", f"/home/{username}/pi-ware/func/term/term-run", str(self.uninstall_script)])
-
+        subprocess.Popen(["bash", f"/home/{username}/pi-ware-pyqt5/func/term/term-run", str(self.uninstall_script)])
 
 if __name__ == '__main__':
      app = QApplication([])
