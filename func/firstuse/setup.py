f2f0461948bf5e811fa64274cb2764ede0824fac
oxmc <67136658+oxmc@users.noreply.github.com>
2021-09-15 20:12:15 -0700

    Update setup.py

--- func/firstuse/setup.py
+++ func/firstuse/setup.py
@@ -1,5 +1,4 @@
-# Pi-Ware setup window
-
+#!/usr/bin/env python3
 from tkinter import *
 from tkinter.ttk import *
 import tkinter as tk
@@ -15,35 +14,33 @@ username = getpass.getuser()
 window = tk.Tk()
 
 #Set window icon
-p1 = PhotoImage(file = f'/home/{username}/pi-ware/icons/logo.png')
+p1 = PhotoImage(file = f'/home/{username}/pi-ware-pyqt5/icons/logo.png')
 window.iconphoto(False, p1)
 
 #Set window title, size, and location
 window.title("Setup Pi-Ware")
-window.geometry("300x200")
+window.geometry("800x500")
 window.eval('tk::PlaceWindow . center')
-
-heading = tk.Label(window, text="""Setup Pi-Ware""", font="Arial 15 bold")
-heading.pack()
 
 def StoreData(Data, Location):
     os.system(f"echo '{Data}' > {Location}")
     window.destroy()
 
-def retrieve_input():
-    inputValue=textBox.get("1.0", "end-1c")
-    #StoreData(inputValue, "$HOME/.local/share/pi-ware/passwd")
+#Define labels
+heading = tk.Label(window, text="""Setup Pi-Ware""", font="Arial 15 bold")
+Thankforinstall = tk.Label(window, text="""Thanks for installing pi-ware!\nLet's set some things up.""", font="Arial 12")
+description = tk.Label(window, text="""Do you want Pi-Ware to upload erros, logs and app info to the pi-ware team?\nPlease note, the pi-ware-team can not use this data to:\ntrack, monitor, or be used to identify you.""", font="Arial 12")
 
-Thankforinstall = tk.Label(window, text="""Thanks for installing pi-ware! \nLet's set some things up.""", font="Arial 12")
+
+#Define buttons
+YesButton=tk.Button(window, height=1, width=10, text="Yes", command=lambda: StoreData("True", "$HOME/.local/share/pi-ware-pyqt5/telemetry"))
+NoButton=tk.Button(window, height=1, width=10, text="No", command=lambda: StoreData("False", "$HOME/.local/share/pi-ware-pyqt5/telemetry"))
+
+#Pack objects
+heading.pack()
 Thankforinstall.pack()
-
-description = tk.Label(window, text="""First enter your password:""", font="Arial 12")
 description.pack()
-
-textBox=tk.Text(window, height=1, width=10)
-textBox.pack()
-
-buttonCommit=tk.Button(window, height=1, width=10, text="Enter", command=lambda: retrieve_input())
-buttonCommit.pack()
+YesButton.pack()
+NoButton.pack()
 
 window.mainloop()
